f5993767589e18cd649f2eda83c0b49a8e1bedad
Tsafack Leslie <leslie.joyce.tsafack.kemvou@mni.thm.de>
2026-03-13 23:51:36 +0100

    Implementierung der Objects-API (CRUD) mit Prepared Statements und Tests

--- java-backend/src/main/java/com/example/object/ObjectRepository.java
+++ java-backend/src/main/java/com/example/object/ObjectRepository.java
@@ -1,12 +1,12 @@
 package com.example.object;
 
 import com.example.database.DatabaseClient;
-
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.jdbcclient.JDBCPool;
 import io.vertx.sqlclient.RowSet;
+import io.vertx.sqlclient.Tuple;
 
 public class ObjectRepository {
 
@@ -16,11 +16,10 @@ public class ObjectRepository {
         this.jdbcPool = DatabaseClient.getInstance();
     }
 
+    /** Fügt neues Object in DB ein. */
     public void insertObject(String message, Handler<AsyncResult<Void>> resultHandler) {
-        int a = 42;
-        // Execute insert query without preparing metadata
-        jdbcPool.query("INSERT INTO objects (message) VALUES ('" + message + "')")
-                .execute(ar -> {
+        jdbcPool.preparedQuery("INSERT INTO objects (message) VALUES (?)")
+                .execute(Tuple.of(message), ar -> {
                     if (ar.succeeded()) {
                         resultHandler.handle(Future.succeededFuture());
                     } else {
@@ -29,16 +28,16 @@ public class ObjectRepository {
                 });
     }
 
+    /** Liefert alle Objects aus der DB. */
     public void fetchObjects(Handler<AsyncResult<RowSet<io.vertx.sqlclient.Row>>> resultHandler) {
-        // Execute select query
         jdbcPool.query("SELECT * FROM objects")
                 .execute(resultHandler);
     }
 
+    /** Aktualisiert Object in DB. */
     public void updateObject(int id, String message, Handler<AsyncResult<Void>> resultHandler) {
-        // Execute update query without preparing metadata
-        jdbcPool.query("UPDATE objects SET message = '" + message + "' WHERE id = " + id)
-                .execute(ar -> {
+        jdbcPool.preparedQuery("UPDATE objects SET message = ? WHERE id = ?")
+                .execute(Tuple.of(message, id), ar -> {
                     if (ar.succeeded()) {
                         resultHandler.handle(Future.succeededFuture());
                     } else {
@@ -47,10 +46,10 @@ public class ObjectRepository {
                 });
     }
 
+    /** Löscht Object aus der DB. */
     public void deleteObject(int id, Handler<AsyncResult<Void>> resultHandler) {
-        // Execute delete query without preparing metadata
-        jdbcPool.query("DELETE FROM objects WHERE id = " + id)
-                .execute(ar -> {
+        jdbcPool.preparedQuery("DELETE FROM objects WHERE id = ?")
+                .execute(Tuple.of(id), ar -> {
                     if (ar.succeeded()) {
                         resultHandler.handle(Future.succeededFuture());
                     } else {
