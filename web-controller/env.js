ed0bf2ef302404d203cd20598864f58ff5c0989b
Jordan Freddy Pokem Tezo <jordan.freddy.pokem.tezo@mni.thm.de>
2026-03-16 18:55:00 +0100

    update MQTT configuration in env.js

--- web-controller/env.js
+++ web-controller/env.js
@@ -1,7 +1,7 @@
 window.__ENV__ = {
-  MQTT_BROKER_URL:      "mosquitto",
+  MQTT_BROKER_URL:      "iti-mqtt.mni.thm.de",
   MQTT_BROKER_PORT:     "9001",
-  MQTT_USERNAME:        "test",
-  MQTT_PASSWORD:        "test1234",
-  MQTT_MESSAGE_PREFIX:  "test/",
+  MQTT_USERNAME:        "group-16",
+  MQTT_PASSWORD:        "r%^e{[FxW8Lp",
+  MQTT_MESSAGE_PREFIX:  "group-16/",
 };
